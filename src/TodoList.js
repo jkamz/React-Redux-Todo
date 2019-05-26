3b31fe4e3fef4b418eaf9d79ec5b452bc653520d
jkamz <jkamz6432@gmail.com>
2019-05-26 06:02:59 +0300

    add form to add todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
+import { connect } from "react-redux";
 
-export default class TodoList extends Component {
+class TodoList extends Component {
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
   render() {
-    const todos = this.state.todos.map((task, index) => (
+    let todos = this.props.todos.map((task, index) => (
       <Todo task={task} key={index} />
     ));
     return (
       <div>
-        <ul>{todos}</ul>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="task">Task</label>
+          <input type="text" name="task" id="task" />
+          <button>Add a Todo</button>
+        </form>
+        <div>
+          <ul>{todos}</ul>
+        </div>
       </div>
     );
   }
 }
+
+function mapStateToProps(reduxState) {
+  return {
+    todos: reduxState.todos
+  };
+}
+
+export default connect(mapStateToProps)(TodoList);
